182d9e601885e30c9d7544ea0b1bb010cd5b000c
mykyyta <nikita.glushko@gmail.com>
2025-01-21 20:50:54 +0200

    hw20 url shortener

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
-from fastapi import FastAPI
+import json
+import random
+import string
+from typing import Annotated
+import aiofiles
+from fastapi import FastAPI, Form, Request
+from starlette.responses import RedirectResponse
+from starlette.templating import Jinja2Templates
 
 app = FastAPI()
-
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root(request: Request):
+    return  templates.TemplateResponse(request, 'index.html')
 
+@app.post("/")
+async def post(request: Request, long_url: Annotated[str, Form()]):
+    short_url = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(5)])
+    async with aiofiles.open('urls.json', 'r') as f:
+        urls_data = json.loads(await f.read())
+    async with aiofiles.open('urls.json','w') as f:
+        urls_data[short_url] = long_url
+        await f.write(json.dumps(urls_data))
+    return {'short_url':short_url}
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/{short_url}")
+async def redirect_to_long_url(short_url: str):
+    async with aiofiles.open("urls.json", "r") as f:
+        urls_data = json.loads(await f.read())
+    long_url = urls_data.get(short_url)
+    if not long_url:
+        return {"error": "Short URL not found"}
+    return RedirectResponse(url=long_url)
